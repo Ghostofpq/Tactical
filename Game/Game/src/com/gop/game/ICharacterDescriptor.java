ffd9367932199159f3e444350fa5b9665f361c60
Ghostofpq <ghostofpq@gmail.com>
2013-05-28 14:07:40 +0200

    ajout de l'interface d'affichage

---
+++ Game/Game/src/com/gop/game/ICharacterDescriptor.java
@@ -0,0 +1,10 @@
+package com.gop.game;
+
+public interface ICharacterDescriptor {
+	public String getName();
+	public String getLevel();
+	public String getRace();
+	
+	public String getHP();
+	public String getMP();
+}
